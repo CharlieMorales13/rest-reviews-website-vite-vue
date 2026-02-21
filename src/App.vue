028599fbcbc429e1a991568d81906a3565db267a
CharlieStroke <stroke.24.13.16@gmail.com>
2026-02-21 16:40:26 -0600

    initialize Vue 3 project with Vite

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import ViteVue from './components/viteVue.vue'
+import Sum from './components/TwoNumberSum.vue'
+</script>
+
+<template>
+  <ViteVue />
+  <Sum />
+  <HelloWorld msg="Vite + Vue" />
+</template>
