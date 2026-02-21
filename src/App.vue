5c5af2c754815eddc37c51df9aca1b980fc06560
CharlieStroke <stroke.24.13.16@gmail.com>
2026-02-21 16:40:58 -0600

    refactor: update component imports and restructure ViteVue component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import ViteVue from './components/viteVue.vue'
-import Sum from './components/TwoNumberSum.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import viteVue from '@/components/ViteVueImgs.vue'
+import Sum from '@/components/TwoNumberSum.vue'
 </script>
 
 <template>
-  <ViteVue />
-  <Sum />
+  <viteVue />
   <HelloWorld msg="Vite + Vue" />
+  <Sum />
 </template>
